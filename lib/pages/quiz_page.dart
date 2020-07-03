8e2400589b99c94d9397dc860c2ecb6ecb23f43a
tina <tinak2@illinois.edu>
2020-07-02 19:53:51 -0500

    Added drop down menu

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -5,48 +5,79 @@ class QuizPage extends StatefulWidget {
   State createState() => new QuizPageState();
 }
 
-class QuizPageState extends State<QuizPage>{
+class QuizPageState extends State<QuizPage> {
   List<String> _diets = [
     'None',
     'Vegan',
     'Gluten Free',
     'Pescaterian',
-    'Ovo-Vegetarian',
-    'Lacto-Vegetarian'
+    'Vegetarian'
   ];
+
+  List<DropdownMenuItem<String>> dropdownDiets;
+  String selectedDiet = "None";
+
+  List<DropdownMenuItem<String>> buildDropdownMenuItems(List<String> diets) {
+    List<DropdownMenuItem<String>> dietItems = List();
+    for (String diet in diets) {
+      dietItems.add(DropdownMenuItem(
+        value: diet, 
+        child: Text(diet)
+        )
+      );
+    }
+    return dietItems;
+  }
+
+  onChangeDropDownItem(String userDiet) {
+    setState(() {
+      selectedDiet = userDiet;
+    }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Questionnaire", style: TextStyle(fontFamily: "MontSerrat", fontSize: 30)),
-        backgroundColor: Color(0xFF7A9BEE),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-              height: 75.0,
-              width: 350.0,
-              color: Colors.transparent,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Color(0xFF7A9BEE), width: 3),
-                  borderRadius: BorderRadius.circular(10.0)
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Questionnaire",
+              style: TextStyle(fontFamily: "MontSerrat", fontSize: 30)),
+          backgroundColor: Color(0xFF7A9BEE),
+        ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Container(
+                height: 75.0,
+                width: 350.0,
+                color: Colors.transparent,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Color(0xFF7A9BEE), width: 3),
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: new Center(
+                    child: new Text("What is your preferred diet?",
+                        style: TextStyle(
+                            color: Color(0xFF7A9BEE),
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Montserrat"),
+                        textAlign: TextAlign.center),
+                  ),
                 ),
-                child: new Center(
-                  child: new Text("What is your preferred diet?", 
-                  style: TextStyle(color: Color(0xFF7A9BEE), fontSize: 23, fontWeight: FontWeight.bold, fontFamily: "Montserrat"),
-                  textAlign: TextAlign.center,),
-                )
+              ),
+              
+              DropdownButton(
+                value: selectedDiet,
+                items: buildDropdownMenuItems(_diets),
+                onChanged: onChangeDropDownItem,
               )
-            )
-          ]
-       )
-      )
+            ]
+          )
+        )
     );
   }
 }
