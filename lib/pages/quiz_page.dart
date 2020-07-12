ce19be4d45f437c57cda6ac65e4ecd207784ac83
Ayushi Srivastava <ayushisrivastava@Ayushis-MBP.hsd1.ca.comcast.net>
2020-07-11 18:12:12 -0700

    Added working slider and next question

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../mechanics/multiselect.dart';
-import '../mechanics/slider.dart';
+import '../mechanics/sliderCircle.dart';
 
 class QuizPage extends StatefulWidget {
   @override
@@ -83,146 +83,122 @@ class QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     
-    return new SingleChildScrollView(
-      child:
-      new Column (
-        children: [
-                    
-        
-        Scaffold(
+    return new Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("Questionnaire",
-              style: TextStyle(fontFamily: "MontSerrat", fontSize: 30)),
-          backgroundColor: Color(0xFF7A9BEE),
+            style: TextStyle(fontFamily: "MontSerrat", fontSize: 30)),
+            backgroundColor: Color(0xFF7A9BEE),
         ),
-        body: Center(
+        body: SingleChildScrollView(
+          child: Center(
             child: Column(
-                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-              new Padding(
-                padding: EdgeInsets.only(top: 50),
-              ),
-              Container(
-                height: 75.0,
-                width: 350.0,
-                color: Colors.transparent,
-                child: Container(
-                  child: new Center(
-                    child: new Text("What is your preferred diet?",
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 25),
+                ),
+                Container(
+                  height: 75.0,
+                  width: 350.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    child: new Center(
+                      child: new Text("What is your preferred diet?",
                         style: TextStyle(
-                            color: Color(0xFF7A9BEE),
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center),
+                          color: Color(0xFF7A9BEE),
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat"),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              DropdownButton(
-                value: selectedDiet,
-                icon: Icon(Icons.arrow_downward),
-                iconSize: 24,
-                elevation: 16,
-                items: buildDropdownMenuItems(_diets),
-                style: TextStyle(color: Color(0xFF7A9BEE), fontSize: 18),
-                underline: Container(
-                  height: 3,
-                  color: Color(0xFF7A9BEE),
+                DropdownButton(
+                  value: selectedDiet,
+                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  items: buildDropdownMenuItems(_diets),
+                  style: TextStyle(color: Color(0xFF7A9BEE), fontSize: 18),
+                    underline: Container(
+                    height: 3,
+                    color: Color(0xFF7A9BEE),
+                  ),
+                  onChanged: onChangeDropDownItem,
                 ),
-                onChanged: onChangeDropDownItem,
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 30),
-              ),
-              Container(
-                height: 75.0,
-                width: 350.0,
-                color: Colors.transparent,
-                child: Container(
-                  child: new Center(
-                    child: new Text("What are your allergies?",
+                Padding(
+                  padding: EdgeInsets.only(bottom: 30),
+                ),
+                Container(
+                  height: 75.0,
+                  width: 350.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    child: new Center(
+                      child: new Text("What are your allergies?",
                         style: TextStyle(
-                            color: Color(0xFF7A9BEE),
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center),
+                          color: Color(0xFF7A9BEE),
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat"),
+                        textAlign: TextAlign.center
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              //Padding(padding: EdgeInsets.only(top: 10)),
-              FloatingActionButton.extended(
-                onPressed: () {
-                  _showMultiSelect(context);
-                },
-                label: Text(
-                  'Open to Select',
-                  style: new TextStyle(color: Colors.white, fontSize: 15),
-                ),
-                backgroundColor: Color(0xFF7A9BEE),
-              ),
-
-              Padding(padding: EdgeInsets.all(20)),
-
-              Container(
-                height: 75.0,
-                width: 350.0,
-                color: Colors.transparent,
-                child: Container(
-                  child: new Center(
-                    child: new Text(
+                //Padding(padding: EdgeInsets.only(top: 10)),
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    _showMultiSelect(context);
+                  },
+                  label: Text('Open to Select', style: new TextStyle(color: Colors.white, fontSize: 15),),
+                  backgroundColor: Color(0xFF7A9BEE),
+                ),      
+                Padding(padding: EdgeInsets.all(20)),
+                Container(
+                  height: 75.0,
+                  width: 350.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    child: new Center(
+                      child: new Text(
                         "Preferred maximum amount of calories in a meal?",
                         style: TextStyle(
-                            color: Color(0xFF7A9BEE),
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center),
+                          color: Color(0xFF7A9BEE),
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat"),
+                        textAlign: TextAlign.center
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 30),
-              ),
-              SliderTheme(
-                data: SliderTheme.of(context).copyWith(
-                  activeTrackColor: Colors.red[700],
-                  inactiveTrackColor: Colors.red[100],
-                  trackShape: RoundedRectSliderTrackShape(),
-                  trackHeight: 4.0,
-                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
-                  thumbColor: Colors.redAccent,
-                  overlayColor: Colors.red.withAlpha(32),
-                  overlayShape: RoundSliderOverlayShape(overlayRadius: 28.0),
-                  tickMarkShape: RoundSliderTickMarkShape(),
-                  activeTickMarkColor: Colors.red[700],
-                  inactiveTickMarkColor: Colors.red[100],
-                  valueIndicatorShape: PaddleSliderValueIndicatorShape(),
-                  valueIndicatorColor: Colors.redAccent,
-                  valueIndicatorTextStyle: TextStyle(
-                    color: Colors.white,
+                Padding(padding: EdgeInsets.all(10)),
+                SliderWidget(48, 0, 2500, false),
+                Padding(padding: EdgeInsets.all(20)),
+                Container(
+                  height: 75.0,
+                  width: 350.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    child: new Center(
+                      child: new Text(
+                        "Any Preexisting Health Conditions?",
+                        style: TextStyle(
+                          color: Color(0xFF7A9BEE),
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat"),
+                        textAlign: TextAlign.center
+                      ),
+                    ),
                   ),
                 ),
-                child: Slider(
-                  min: 0,
-                  max: 2500,
-                  divisions: 10,
-                  value: sliderValue,
-                  label: '$sliderValue',
-                  onChanged: (value) {
-                    setState(() {
-                      sliderValue = value;
-                    });
-                  },
-                ),
-              )
-            ]
-          )
-        )
-      )
-      ]
-    )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
