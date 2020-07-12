918a209573fe74c067b86d0ed1dbafacfb27536a
Ayushi Srivastava <ayushisrivastava@Ayushis-MBP.hsd1.ca.comcast.net>
2020-07-11 18:14:38 -0700

    Adding slider files

---
+++ lib/mechanics/sliderBar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class CustomSliderThumbCircle extends SliderComponentShape {
+  final double thumbRadius;
+  final double thumbHeight;
+  final int min;
+  final int max;
+
+  CustomSliderThumbCircle (
+    {
+    this.thumbRadius,
+    this.thumbHeight,
+    this.min,
+    this.max
+    }
+);
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return Size.fromRadius(thumbRadius);
+  }
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    Animation<double> activationAnimation,
+    Animation<double> enableAnimation,
+    bool isDiscrete,
+    TextPainter labelPainter,
+    RenderBox parentBox,
+    SliderThemeData sliderTheme,
+    TextDirection textDirection,
+    double value,
+  }) {
+    final Canvas canvas = context.canvas;
+
+    final paint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+
+    TextSpan span = new TextSpan(
+      style: new TextStyle(
+        fontSize: thumbRadius * .7,
+        fontWeight: FontWeight.w700,
+        color: sliderTheme.thumbColor,
+      ),
+      text: getValue(value),
+    );
+
+    TextPainter tp = new TextPainter(
+        text: span,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+        
+    tp.layout();
+    Offset textCenter = Offset(center.dx - (tp.width / 2), center.dy - (tp.height / 2));
+
+    canvas.drawCircle(center, thumbRadius * .9, paint);
+    tp.paint(canvas, textCenter);
+  }
+
+  String getValue(double value) {
+    return ((max * value).round()).toString();
+  }
+}
