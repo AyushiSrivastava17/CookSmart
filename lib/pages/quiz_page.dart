4b012be73ab1fb794d27ac71ded1455099515b3f
tina <tinak2@illinois.edu>
2020-07-13 21:48:35 -0500

    Fixed button color selection in health condition question

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -8,6 +8,12 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage> {
+  Color _noneColor = Colors.indigo[50];
+  Color _diabetesColor = Colors.indigo[50];
+  Color _highBloodPressureColor = Colors.indigo[50];
+  Color _lowBloodPressureColor = Colors.indigo[50];
+  Color _highCholestrolColor = Colors.indigo[50];
+
   double sliderValue = 0;
   List<MultiSelectDialogItem<int>> multiItem = List();
 
@@ -28,7 +34,7 @@ class QuizPageState extends State<QuizPage> {
     }
   }
 
-  void _showMultiSelect(BuildContext context, Set<int> selected) async{
+  void _showMultiSelect(BuildContext context, Set<int> selected) async {
     multiItem = [];
     populateMultiselect();
     final items = multiItem;
@@ -84,233 +90,290 @@ class QuizPageState extends State<QuizPage> {
 
   @override
   Widget build(BuildContext context) {
-    Set<int> selected = Set(); 
-    bool pressAttention = false; 
+    Set<int> selected = Set();
     return new Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Questionnaire",
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Questionnaire",
             style: TextStyle(fontFamily: "MontSerrat", fontSize: 30)),
-            backgroundColor: Color(0xFF7A9BEE),
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 25),
+        backgroundColor: Color(0xFF7A9BEE),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 25),
+            ),
+            Container(
+              height: 75.0,
+              width: 350.0,
+              color: Colors.transparent,
+              child: Container(
+                child: new Center(
+                  child: new Text(
+                    "What is your preferred diet?",
+                    style: TextStyle(
+                        color: Color(0xFF7A9BEE),
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                Container(
-                  height: 75.0,
-                  width: 350.0,
-                  color: Colors.transparent,
-                  child: Container(
-                    child: new Center(
-                      child: new Text("What is your preferred diet?",
-                        style: TextStyle(
+              ),
+            ),
+            DropdownButton(
+              value: selectedDiet,
+              icon: Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              items: buildDropdownMenuItems(_diets),
+              style: TextStyle(color: Color(0xFF7A9BEE), fontSize: 18),
+              underline: Container(
+                height: 3,
+                color: Color(0xFF7A9BEE),
+              ),
+              onChanged: onChangeDropDownItem,
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 30),
+            ),
+            Container(
+              height: 75.0,
+              width: 350.0,
+              color: Colors.transparent,
+              child: Container(
+                child: new Center(
+                  child: new Text("What are your allergies?",
+                      style: TextStyle(
                           color: Color(0xFF7A9BEE),
                           fontSize: 23,
                           fontWeight: FontWeight.bold,
                           fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
+                      textAlign: TextAlign.center),
                 ),
-                DropdownButton(
-                  value: selectedDiet,
-                  icon: Icon(Icons.arrow_downward),
-                  iconSize: 24,
-                  elevation: 16,
-                  items: buildDropdownMenuItems(_diets),
-                  style: TextStyle(color: Color(0xFF7A9BEE), fontSize: 18),
-                    underline: Container(
-                    height: 3,
-                    color: Color(0xFF7A9BEE),
-                  ),
-                  onChanged: onChangeDropDownItem,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 30),
-                ),
-                Container(
-                  height: 75.0,
-                  width: 350.0,
-                  color: Colors.transparent,
-                  child: Container(
-                    child: new Center(
-                      child: new Text("What are your allergies?",
-                        style: TextStyle(
+              ),
+            ),
+            //Padding(padding: EdgeInsets.only(top: 10)),
+            FloatingActionButton.extended(
+              onPressed: () {
+                if (selected.isEmpty) {
+                  selected.add(1);
+                }
+                _showMultiSelect(context, selected);
+                //getvaluefromkey(selected);
+              },
+              label: Text(
+                'Open to Select',
+                style: new TextStyle(color: Colors.white, fontSize: 15),
+              ),
+              backgroundColor: Color(0xFF7A9BEE),
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            Container(
+              height: 75.0,
+              width: 350.0,
+              color: Colors.transparent,
+              child: Container(
+                child: new Center(
+                  child: new Text(
+                      "Preferred maximum amount of calories in a meal?",
+                      style: TextStyle(
                           color: Color(0xFF7A9BEE),
                           fontSize: 23,
                           fontWeight: FontWeight.bold,
                           fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center
-                      ),
-                    ),
-                  ),
+                      textAlign: TextAlign.center),
                 ),
-                //Padding(padding: EdgeInsets.only(top: 10)),
-                FloatingActionButton.extended(
-                  onPressed: () {
-                     if (selected.isEmpty){
-                        selected.add(1);
-                     }
-                    _showMultiSelect(context, selected);
-                    //getvaluefromkey(selected);
-                  },
-                  label: Text('Open to Select', style: new TextStyle(color: Colors.white, fontSize: 15),),
-                  backgroundColor: Color(0xFF7A9BEE),
-                ),      
-                Padding(padding: EdgeInsets.all(20)),
-                Container(
-                  height: 75.0,
-                  width: 350.0,
-                  color: Colors.transparent,
-                  child: Container(
-                    child: new Center(
-                      child: new Text(
-                        "Preferred maximum amount of calories in a meal?",
-                        style: TextStyle(
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+
+            SliderWidget(48, 0, 2500, false),
+
+            Padding(padding: EdgeInsets.all(20)),
+
+            Container(
+              height: 75.0,
+              width: 350.0,
+              color: Colors.transparent,
+              child: Container(
+                child: new Center(
+                  child: new Text("Any Preexisting Health Conditions?",
+                      style: TextStyle(
                           color: Color(0xFF7A9BEE),
                           fontSize: 23,
                           fontWeight: FontWeight.bold,
                           fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center
-                      ),
-                    ),
+                      textAlign: TextAlign.center),
+                ),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(10)),
+
+            RaisedButton(
+              color: _noneColor,
+              onPressed: () {
+                setState(() {
+                  if (_noneColor == Colors.indigo[50]) {
+                    _noneColor = Colors.blue[100];
+                  } else {
+                    _noneColor = Colors.indigo[50];
+                  }
+                });
+              },
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(15)),
+              child: Container(
+                width: 300,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'None',
+                    style: TextStyle(
+                        color: Color(0xFF7A9BEE),
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                Padding(padding: EdgeInsets.all(10)),
-                SliderWidget(48, 0, 2500, false),
-                Padding(padding: EdgeInsets.all(20)),
-                Container(
-                  height: 75.0,
-                  width: 350.0,
-                  color: Colors.transparent,
-                  child: Container(
-                    child: new Center(
-                      child: new Text(
-                        "Any Preexisting Health Conditions?",
-                        style: TextStyle(
-                          color: Color(0xFF7A9BEE),
-                          fontSize: 23,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center
-                      ),
-                    ),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(5)),
+
+            RaisedButton (
+              color: _diabetesColor,
+              onPressed: () {
+                setState(() {
+                  if (_diabetesColor == Colors.indigo[50]) {
+                    _diabetesColor = Colors.blue[100];
+                  } else {
+                    _diabetesColor = Colors.indigo[50];
+                  }
+                });
+              },
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(15)),
+              child: Container(
+                width: 300,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Type 2 Diabetes',
+                    style: TextStyle(
+                        color: Color(0xFF7A9BEE),
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                Card(
-                  color: Colors.indigo[50],
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    onTap: () {
-                      print('"None" health condition tapped.');
-                    },
-                    child: Container(
-                      width: 300,
-                      height: 50,
-                      child: Center(
-                        child: Text('None',
-                          style: TextStyle(
-                            color: Color(0xFF7A9BEE),
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"),
-                          textAlign: TextAlign.center,),
-                      ),
-                    ),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(5)),
+
+            RaisedButton(
+              color: _highBloodPressureColor,
+              onPressed: () {
+                setState(() {
+                  if (_highBloodPressureColor == Colors.indigo[50]) {
+                    _highBloodPressureColor = Colors.blue[100];
+                  } else {
+                    _highBloodPressureColor = Colors.indigo[50];
+                  }
+                });
+              },
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(15)),
+              child: Container(
+                width: 300,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'High Blood Pressure',
+                    style: TextStyle(
+                        color: Color(0xFF7A9BEE),
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                Card(
-                  color: Colors.indigo[50],
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    onTap: () {
-                      print('"Diabetic" health condition tapped.');
-                    },
-                    child: Container(
-                      width: 300,
-                      height: 50,
-                      child: Center(
-                        child: Text('Type 2 Diabetes',
-                          style: TextStyle(
-                            color: Color(0xFF7A9BEE),
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"),
-                          textAlign: TextAlign.center,),
-                      ),
-                    ),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(5)),
+
+            RaisedButton(
+              color: _lowBloodPressureColor,
+              onPressed: () {
+                setState(() {
+                  if (_lowBloodPressureColor == Colors.indigo[50]) {
+                    _lowBloodPressureColor = Colors.blue[100];
+                  } else {
+                    _lowBloodPressureColor = Colors.indigo[50];
+                  }
+                });
+              },
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(15)),
+              child: Container(
+                width: 300,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Low Blood Pressure',
+                    style: TextStyle(
+                        color: Color(0xFF7A9BEE),
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                Card(
-                  color: Colors.indigo[50],
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    onTap: () {
-                      print('"High BP" health condition tapped.');
-                    },
-                    child: Container(
-                      width: 300,
-                      height: 50,
-                      child: Center(
-                        child: Text('High Blood Pressure',
-                          style: TextStyle(
-                            color: Color(0xFF7A9BEE),
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"),
-                          textAlign: TextAlign.center,),
-                      ),
-                    ),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(5)),
+
+          RaisedButton(
+              color: _highCholestrolColor,
+              onPressed: () {
+                setState(() {
+                  if (_highCholestrolColor == Colors.indigo[50]) {
+                    _highCholestrolColor = Colors.blue[100];
+                  } else {
+                    _highCholestrolColor = Colors.indigo[50];
+                  }
+                });
+              },
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(15)),
+              child: Container(
+                width: 300,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'High Cholestrol',
+                    style: TextStyle(
+                        color: Color(0xFF7A9BEE),
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                RaisedButton(
-                  color: pressAttention ? Colors.grey : Colors.indigo[50],
-                  onPressed: () => setState(() => pressAttention = !pressAttention),
-                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(15)),
-                  highlightColor: Colors.grey,
-                  child: Container(
-                    width: 300,
-                    height: 50,
-                    child: Center(
-                      child: Text('Low Blood Pressure',
-                        style: TextStyle(
-                          color: Color(0xFF7A9BEE),
-                          fontSize: 23,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: "Montserrat"),
-                        textAlign: TextAlign.center,),
-                    ),
-                  ),
-                ),
-                Card(
-                  color: Colors.indigo[50],
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    onTap: () {
-                      print('"High Cholesterol" health condition tapped.');
-                    },
-                    child: Container(
-                      width: 300,
-                      height: 50,
-                      child: Center(
-                        child: Text('High Cholesterol',
-                          style: TextStyle(
-                            color: Color(0xFF7A9BEE),
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: "Montserrat"),
-                          textAlign: TextAlign.center,),
-                      ),
-                    ),
-                  ),
-                )
-              ]
-          ),
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(5)),
+
+
+          ]),
         ),
       ),
     );
